1155000b1d36208df86408846c1bbe335378aefd
Vali_Solo <solotych@gmail.com>
2019-09-12 13:10:21 +0200

    P03 und P04 Update

--- P03_Funktionen.py
+++ P03_Funktionen.py
@@ -1,11 +1,8 @@
-
 # Aufgabe 1
 """Erstellen Sie folgende Variablen in Python mit den Entsprechenden Werten
 x=1
 y=2
-z=3
-"""
-
+z=3"""
 
 
 # Aufgabe 2
@@ -15,135 +12,109 @@ mit:
 x=1
 y=2
 z=3
-"""
-x=1
-y=2
-z=3
-f=(x**2)+ y**3 + z**4
+Ueberlegen Sie sich, was passiert wenn Sie die Funktion f immer wieder verwenden
+muessen, aber mit wechselnden Eingangsparameter x,y,z"""
 
-# Aufgabe 3
-"""
-Loeschen Sie alle Variablen im Workspace.
+
+# Aufgabe 3 (Definieren einer Funktion)
+"""Loeschen Sie alle Variablen im Workspace.
 Definieren Sie eine Funktion in Python mit dem Funktionsnamen 'func1', 
 Eingangsparamter sind x, y und z
 Im Funktionskörper wird folgendes berechnet
 f=(x**2)+ y**3 + z**4
 Ausgabewert ist f
-Hier nochmal das Beispiel von der Powerpoint Folie
-"""
+Hier nochmal das Beispiel von der Powerpoint Folie"""
 def myfunc(a,b):
     c=a+b
     return(c)
 
 
-# Aufgabe 4
-"""
-Nehmen Sie sich die Funktion von Aufgabe 3 zur Hilfe und Berechnen Sie folgenden
-Wert: f=(1**2)+ 2**3 + 3**4
-"""    
+# Aufgabe 4 (Ausfuehren einer Funktion)
+"""Nehmen Sie sich die Funktion von Aufgabe 3 zur Hilfe und Berechnen Sie f, 
+wenn f folgendermassen definiert ist: f=(1**2)+ 2**3 + 3**4"""    
 
 
-
-# Aufgabe 5
-"""Definieren Sie eine Funktion in Python mit dem Funktionsnamen func2, 
-Eingangsparamter sind x, y und z
-Im Funktionskörper wird folgendes berechnet
- für x>0
+# Aufgabe 5 (If-Else in einer Funktion)
+"""Definieren Sie sich eine Funktion in Python mit dem Funktionsnamen func2, 
+Eingangsparamter sind x, y und z. Im Funktionskörper wird folgendes berechnet:
+f=x**2+ y**3 + z**4 für x>0
 und
 f= y**3 + z**4 für x<=0
-Ausgabewert ist f
-"""
-
+Ausgabewert ist f. Verwenden Sie eine if-else Abfrage im Funktionskoerper"""
 
 
 #Aufgabe 6:
-"""
-Nehmen Sie sich die Funktion von Aufgabe 5 zur Hilfe und Berechnen Sie folgenden
-Werte:
+"""Nehmen Sie sich die Funktion von Aufgabe 5 zur Hilfe und Berechnen Sie  
+folgenden Werte:
 g1=func2(3,4,5)
 g2=func2(-3,4,5)
 
-g1 und g2 sind dabei neue Variablen, denen Sie die Funktionswerte zuweisen.
-"""    
+g1 und g2 sind dabei neue Variablen, denen Sie die Funktionswerte zuweisen."""    
 
-#Aufgabe 7
-"""
-Definieren Sie eine Funktion in Python mit dem Funktionsnamen func, Eingangsparamter 
-sind x, y und z
-Im Funktionskörper wird folgendes berechnet
+#Aufgabe 7 (Funktion mit mehreren Ausgabewerten)
+"""Definieren Sie eine Funktion in Python mit dem Funktionsnamen func, 
+Eingangsparamter sind x, y und z. Im Funktionskörper wird folgendes berechnet:
 f=(x**2)+ y**3 + z**4 
 und
 g= y**3 + z**4 
-Ausgabewerte sind f und g
-
-"""
+Ausgabewerte sind f und g"""
 
 
-#Aufgabe 8
-"""
-Verwenden Sie die Funktion aus Aufgabe 7, mit x=1,y=2 und z=3
+#Aufgabe 8 
+"""Verwenden Sie die Funktion aus Aufgabe 7, mit x=1,y=2 und z=3
 1. Speichern Sie sich f und g gleichzeitig in die Variablen a11 und a12 ab
-2. Speichern Sie sich nur f in die Variable a2
+2. Speichern Sie sich nur f in die Variable a2"""
 
 
-"""
-
-# Aufgabe 9
-"""
-Definieren Sie eine Funktion in Python mit dem Funktionsnamen func, Eingangsparamter 
-sind x, y und z
-Im Funktionskörper wird folgendes berechnet
+# Aufgabe 9 (Funktionen ohne Ausgabe)
+"""Definieren Sie eine Funktion in Python mit dem Funktionsnamen func, 
+Eingangsparamter sind x, y und z. Im Funktionskörper wird folgendes berechnet:
 f=(x**2)+ y**3 + z**4 für x>0
 und
 f= y**3 + z**4 für x<=0
 Sie brauchen keinen Ausgabewert, aber lassen Sie das Ergebnis mit der print 
-Funktion anzeigen
-"""
+Funktion anzeigen"""
 
-# Aufgabe 10
-"""
-Definieren Sie eine Funktion in Python mit dem Funktionsnamen func, Eingangsparamter 
-sind x, y und z
-Im Funktionskörper wird folgendes berechnet
+
+# Aufgabe 10 (Funktionen mit standart (default) Eingabewerten)
+"""Definieren Sie eine Funktion in Python mit dem Funktionsnamen func, 
+Eingangsparamter sind x, y und z. Im Funktionskörper wird folgendes berechnet
 f=(x**2)+ y**3 + z**4 
-Ausgabewerte ist f.
-Erstellen Sie die Funktion so, dass der User 
-nur den Parameter fuer x Angeben muss. Falls 
-dies der Fall ist nehmen die restlichen Eingangsparameter folgende Werte an
+Ausgabewert ist f.
+Erstellen Sie die Funktion so, dass der User nur den Parameter fuer x Angeben
+kann. Zum Beispiel: func(10).
+Falls dies der Fall ist nehmen die restlichen Eingangsparameter folgende 
+Werte an:
 y=0
 z=0
-"""
+Unser Beispiel func(10) wuerde folgendes Ergebniss liefern: 100 """
+
 
 #Aufgabe 11
-"""
-Verwenden Sie die Funktion aus Aufgabe 10, um den Funktionswert f zu
-berechnen. Verwenden Sie folgende Eingangsparameter mit x=1,y=2
+"""Verwenden Sie die Funktion aus Aufgabe 10, um den Funktionswert f zu
+berechnen. Verwenden Sie folgende Eingangsparameter mit x=1,y=2"""
 
-"""
 
-#Aufgabe 12
-"""
-Loeschen Sie alle Variablen im Workspace.
+#Aufgabe 12 (global vs local Variablen)
+"""Loeschen Sie alle Variablen im Workspace.
 Gegeben ist die Funktion mit dem Namen "myfunc". Fuehren Sie den unten stehenden 
 Code aus. Beobachten Sie welche Variablen in Ihrem Workspace angelegt werden.
 Aendern Sie die Funktion "myfunc" so, dass nach dem Durchlaufen des codes, die 
-Variable "c" im Workspace erscheint. 
-"""
+Variable "c" im Workspace erscheint. """
 def myfunc(a,b):
     c=a+b
     return(c) 
 
 result=myfunc(5,10)
 
+
 #Aufgabe 13
-"""
-Loeschen Sie alle Variablen im Workspace.
-Gegeben ist die Funktion mit dem Namen "myfunc2". Fuehren Sie den unten stehenden 
-Code aus. Beobachten Sie welche Variablen in Ihrem Workspace angelegt werden.
-Welchen Wert haben die Variablen "a", "b" und "c" am Ende, warum?
+"""Loeschen Sie alle Variablen im Workspace.
+Gegeben ist die Funktion mit dem Namen "myfunc2". Fuehren Sie den unten 
+stehenden Code aus. Beobachten Sie welche Variablen in Ihrem Workspace angelegt 
+werden. Welchen Wert haben die Variablen "a", "b" und "c" am Ende, warum?
 Aendern Sie die Funktion "myfunc2" so, dass nach dem Durchlaufen des codes, die 
-Variable "c" den Wert aus der Funktion hat. 
-"""
+Variable "c" den Wert aus der Funktion hat (15). """
 a=1
 b=2
 c=3
@@ -154,9 +125,37 @@ def myfunc2(a,b):
 
 result=myfunc2(5,10)
 
-#Aufgabe 14
-"""
-Weisen Sie der Varibale x den Satz "Hello World" zu, indem Sie eine Eingabe 
+#Aufgabe 14 (globa vs local Variablen fuer Fortgeschrittene)
+"""Fuehren Sie den Code der unten steht aus. """
+a = 1
+
+# Verwendet globale Variable, da es kein lokales 'a' existiert.
+def f(): 
+	print ('Inside f() : ', a )
+
+# Variable 'a' wird lokal definiert und wird in der Funktion verwendet
+def g():	 
+	a = 2
+	print ('Inside g() : ',a )
+
+# Variable wird in der Funktion als global deklariert und ueberschrieben
+def h():	 
+	global a 
+	a = 3
+	print ('Inside h() : ',a) 
+
+"""Ab hier jede Zeile einzeln ausfuehren. Beobachten Sie was mit der Variablen
+mit dem Namen 'a' passiert."""
+print (a)
+f() 
+print (a)
+g() 
+print (a)
+h() 
+print (a)
+
+
+#Aufgabe 15 (input Funktion)
+"""Weisen Sie der Varibale x den Satz "Hello World" zu, indem Sie eine Eingabe 
 von dem User in der Konsole aufnehmen. Verwenden Sie die "input()" Funktion 
-Geben Sie die Eingabe in der Konsole aus.
-"""
+Geben Sie die Eingabe in der Konsole aus."""
--- P04_Iterationen.py
+++ P04_Iterationen.py
@@ -1,127 +1,119 @@
-# Aufgabe 1
-"""
-Fuehren Sie den unten stehenden Code auf einmal aus. Beobachten Sie die 
-Ausgabe in der Konsole. Welche Zahlen werden ausgegeben.
-"""
+# Aufgabe 1 (for - Schleife durch eine Liste)
+"""Fuehren Sie den unten stehenden Code auf einmal aus. Beobachten Sie die 
+Ausgabe in der Konsole. Welche Zahlen werden ausgegeben."""
 for i in [1,2,3,4,5]:
     print (i)
 print("alles fertig")
 
-# Aufgabe 2
-"""
-Fuehren Sie den unten stehenden Code auf einmal aus. Beobachten Sie die 
-Ausgabe in der Konsole. Welche Ausgabe sehen Sie?
-"""
+
+# Aufgabe 2 (for - Scheife durch einen String)
+"""Fuehren Sie den unten stehenden Code auf einmal aus. Beobachten Sie die 
+Ausgabe in der Konsole. Welche Ausgabe sehen Sie?"""
 for i in "hello World":
     print (i)
 print ("alles fertig")
 
-# Aufgabe 3
-"""
-Fuehren Sie den unten stehenden Code auf einmal aus. Beobachten Sie die 
-Ausgabe in der Konsole. Welche Ausgabe sehen Sie?
-"""
+
+# Aufgabe 3 (for - Schleife mit der range Funktion)
+"""Fuehren Sie den unten stehenden Code auf einmal aus. Beobachten Sie die 
+Ausgabe in der Konsole. Welche Ausgabe sehen Sie?"""
 for i in range(5):
     print (i)    
 print ("alles fertig")
 
-# Aufgabe 4
-"""
-Erstellen Sie eine for-Schleife, die Zahlen von 0 bis 5 ausgibt. Bei der Zahl 3
-wird folgender Text ausgegeben: "Alle guten Dinge sind drei"
+
+# Aufgabe 4 (Schleife mit if-Abfrage)
+"""Erstellen Sie eine for-Schleife, die Zahlen von 0 bis 5 ausgibt. Bei der 
+Zahl 3 wird folgender Text ausgegeben: "Alle guten Dinge sind drei"
 Verwenden Sie eine if-Abfrage und die range Funktion, um die Aufgabe
-umzusetzten. 
-"""
+umzusetzten. """
 
-# Aufgabe 5
-"""
-Erstellen Sie eine for-Schleife, die Zahlen von 0 bis 10 aufsummiert. 
-"""
 
-# Aufgabe 6
-"""
-Erstellen Sie eine for-Schleife, die Zahlen von 5 bis 10 ausgibt. Verwenden Sie
-die range Funktion, aehnlich wie in Aufgabe 3.
-Suchen Sie im Internet wie man die range-Funktion so aendert das die gewuenschten
-Zahlen ausgegeben werden. 
-"""
+# Aufgabe 5 (Schleife die aufsummiert)
+"""Erstellen Sie eine for-Schleife, die Zahlen von 0 bis 10 aufsummiert. """
+
+
+# Aufgabe 6 (Modifikation der range-Funktion)
+"""Erstellen Sie eine for-Schleife, die Zahlen von 5 bis 10 ausgibt. Verwenden 
+Sie die range Funktion, aehnlich wie in Aufgabe 3.
+Suchen Sie im Internet wie man die range-Funktion so aendert das die 
+gewuenschten Zahlen ausgegeben werden. """
+
 
 # Aufgabe 7
-"""
-Erstellen Sie eine for-Schleife, die Zahlen von 5 bis 20 ausgibt, aber nur ungerade
-Zahlen. 
+"""Erstellen Sie eine for-Schleife, die Zahlen von 5 bis 20 ausgibt, aber nur 
+ungerade Zahlen. 
 Verwenden Sie die range Funktion, aehnlich wie in Aufgabe 3.
-Suchen Sie im Internet wie man die range-Funktion so aendert das die gewuenschten
-Zahlen ausgegeben werden. 
+Suchen Sie im Internet wie man die range-Funktion so aendert das die 
+gewuenschten Zahlen ausgegeben werden. 
 Bonus Aufgabe fuer die Pros: Verwenden Sie range(20) fuer die Definition der
-Schleife und loesen Sie trotzdem die Aufgabe.
-"""
+Schleife und loesen Sie trotzdem die Aufgabe."""
+
 
 # Aufgabe 8
-"""
-Erstellen Sie eine for-Schleife, die Zahlen von 10 bis 0 (10, 9, 8, 7, 6...) ausgibt. 
-Also im vergleich zu Aufgabe 3, rueckwaerts. Verwenden Sie die range Funktion.
-Suchen Sie im Internet wie man die range-Funktion so aendert das die Zahlen in 
-umgekehrter Reihenfolge ausgegeben werden.
+"""Erstellen Sie eine for-Schleife, die Zahlen von 10 bis 0 (10, 9, 8, 7, 6...) 
+ausgibt. Also im vergleich zu Aufgabe 3, rueckwaerts. Verwenden Sie die range 
+Funktion. Suchen Sie im Internet wie man die range-Funktion so aendert das die 
+Zahlen in umgekehrter Reihenfolge ausgegeben werden.
 Bonus Aufgabe fuer die Pros: Verwenden Sie range(11) fuer die Definition der
 Schleife und loesen Sie trotzdem die Aufgabe.
 """
 
-# Aufgabe 9
-"""
-Fuehren Sie den unten stehenden Code auf einmal aus. Es handelt sich hierbei um
-eine einfache while-Schleife
-"""
+# Aufgabe 9 (While-Schleife Einfuehrung)
+"""Fuehren Sie den unten stehenden Code auf einmal aus. Es handelt sich hierbei 
+um eine einfache while-Schleife. Vergleichen Sie die while-Schleife mit der 
+for-Schleife. Welche Unterschiede erkennen Sie?"""
 x=4
 
 while x<10:
-    x= x+1  # here we changing the condition
+    x= x+1  
     print(x)
-print("now x is no longer smaller than 10")
+print("Jetzt ist x nicht mehr kleiner als 10")
 
 
-# Aufgabe 10
-"""
-Fuehren Sie den unten stehenden Code auf einmal aus. Es handelt sich hierbei um
-eine einfache endlos while-Schleife.
-Verwenden Sie Strg+c um die endlos Schleife abzubrechen.
-"""
+# Aufgabe 10 (Endlos Schleife)
+"""Fuehren Sie den unten stehenden Code auf einmal aus. Es handelt sich hierbei 
+um eine einfache endlos while-Schleife.
+Verwenden Sie Strg+c um die endlos Schleife abzubrechen. Aendern Sie den code 
+so um, dass die while-Schleife keine endlos Schleife mehr ist. Tipp: Es gibt 
+mehrere Moeglichkeiten dies zu tun."""
 x=11
 while x>10:
     print("Help!!")
     
 print("we will never finish")
 
-# Aufgabe 11
-"""
-Ergaenzen Sie folgenden Code so, dass der Variablen x immer 1 hinzugefuegt wird,
-bis x groesser als 50 ist. Lassen Sie nach jeder Addition das Ergebniss mit einer
-print-Funktion ausgeben.
-"""
+
+# Aufgabe 11 (Inkrementieren in einer while-Schleife)
+"""Ergaenzen Sie folgenden Code so, dass der Variablen x immer 1 hinzugefuegt 
+wird, bis x groesser als 50 ist. Lassen Sie nach jeder Addition das Ergebniss 
+mit einer print-Funktion ausgeben.
+Gegeben ist ein Beispiel wie man x von 0 auf 1 erhoeht. Sie muessen nun dieses
+Beispiel in eine Schleife einfuegen."""
 x=0
 increment = 1
 x=x+increment
 print(x)
 
-# Aufgabe 12
-"""
-Ergaenzen Sie folgenden Code aus Aufgabe 12 so, dass die Variable x immer ein 
-beliebiger Wert hinzugefuegt wird,
-bis x groesser als 50 ist. Lassen Sie nach jeder Addition das Ergebniss mit einer
+
+# Aufgabe 12 
+"""Ergaenzen Sie folgenden Code aus Aufgabe 12 so, dass die Variable x immer 
+ein beliebiger Wert hinzugefuegt wird, bis x groesser als 50 ist. Der beliebige
+Wert wird jedesmal per input-Funktion abgefragt.
+Lassen Sie nach jeder Addition das Ergebniss mit einer
 print-Funktion ausgeben. Verwenden Sie eine while-Schleife.
-Bonus Aufgabe fuer die Pros: Verwenden Sie eine for-Schleife
-"""
+Bonus Aufgabe fuer die Pros: Verwenden Sie eine for-Schleife"""
 x=0
 increment = int(input("Geben Sie eine ganze Zahl zwischen 1 und 10 ein:"))
 x=x+increment
 print(x)
 
-#Aufgabe 13
-"""
-Fuehren Sie den unten stehenden Code auf einmal aus. Es handelt sich hierbei um
-eine einfache endlos while-Schleife mit eienm break statement. Machen Sie mehrere Eingaben.
-Welche Eingabe muessen Sie taetigen um aus die endlos Schleife zu beenden.
-"""
+
+#Aufgabe 13 (Break-Statement)
+"""Fuehren Sie den unten stehenden Code auf einmal aus. Es handelt sich hierbei 
+um eine einfache endlos while-Schleife mit eienm break statement. Machen Sie 
+mehrere Eingaben. Welche Eingabe muessen Sie taetigen um die endlos 
+Schleife zu beenden."""
 x=4
 while x>3:
     zeile= input("Neue Eingabe:\n")
@@ -130,10 +122,10 @@ while x>3:
     print("Ihre Eingabe:"+zeile)
 print("Skript ist beendet")
 
-#Aufgabe 14
-"""
-Fuehren Sie den unten stehenden Code auf einmal aus. Es handelt sich hierbei um
-eine einfache endlos while-Schleife mit eienm continue und break statement. 
+
+#Aufgabe 14 (continue-Statement)
+"""Fuehren Sie den unten stehenden Code auf einmal aus. Es handelt sich hierbei 
+um eine einfache endlos while-Schleife mit eienm continue und break statement. 
 Machen Sie mehrere Eingaben in der Konsole.
 Welche Eingabe muessen Sie taetigen damit Ihre Eingabe nicht in der Konsole
 ausgegeben wird, Sie aber trotzdem in der while- Schleife bleiben.
@@ -151,11 +143,11 @@ print("Skript ist beendet")
    
 
 #Aufgabe 15
-""" Gegeben ist eine Ansammlung von Zahlen :[3, 43,58, 84,30,-2]. Ergaenzen Sie 
-den Code so, das die groesste Zahl am Ende ausgegeben wird. Gehen Sie davon aus, 
-das Sie keine Einsicht in die Ansammlung von Zahlen haben. Ihr Code muss selbst-
-aendig die groesste Zahl finden. (Sie koennen davon ausgehen das die kleinste 
-Zahl -10000 ist.)
+""" Gegeben ist eine Ansammlung von Zahlen(Liste):[3, 43,58, 84,30,-2]. 
+Ergaenzen Sie den Code so, das die groesste Zahl am Ende ausgegeben wird. Gehen 
+Sie davon aus, das Sie keine Einsicht in die Ansammlung von Zahlen haben. Ihr 
+Code muss selbstaendig die groesste Zahl finden. (Sie koennen davon ausgehen 
+das die kleinste Zahl -10000 ist.)
 Bonus Aufgabe fuer die Pros: Loesen Sie die Aufgabe mit einer while-Schleife
 """
 for i in [3, 43,58, 84,30,-2]:
@@ -177,13 +169,12 @@ for i in [3, 43,3,58, 84,3,30,-2,3]:
 print("Number of times, 3 appeared:" + str(count))
 
 
-#Aufgabe 16
-""" Ergaenzen Sie den Code so, dass jeder Buchstabe in der Konsole ausgegeben wird
-aehnlich wie in Aufgabe 2. Ersetzten Sie dabei den Buchstaben 'l' durch ein 'r' 
-ersetzt wird.
-Bonus Aufgabe fuer die Pros: Geben Sie am Ende den kompletten Satz in der Konsole
-wieder, mit den geaenderten Buchstaben.
-"""
+#Aufgabe 17
+""" Ergaenzen Sie den Code so, dass jeder Buchstabe einzeln in der Konsole 
+ausgegeben wird, aehnlich wie in Aufgabe 2. Ersetzten Sie dabei den Buchstaben 
+'l' durch ein 'r'.
+Bonus Aufgabe fuer die Pros: Geben Sie am Ende den kompletten Satz in der 
+Konsole wieder, mit den geaenderten Buchstaben."""
 x=input("Geben Sie einen Satz ein:\n")
 
 

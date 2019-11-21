c82d325f970d768838baf02d26b0055e73eebb54
valentin solotych <valentin.solotych@iav.de>
2019-11-21 16:25:27 +0100

    Revert "P06 Loesung"
    This reverts commit 4cccf82e6d0b05cec31e153c93a09ed24d3784e1.

--- P06_Listen.py
+++ P06_Listen.py
@@ -6,17 +6,14 @@ Erstellen sie eine Liste mit dem Namen "friends" die 3 strings enthaelt.
 Die drei strings sind Namen: Andi, Michi, Anna
 Hier ist ein Beispiel wie man eine Liste erstellt mit  3 Zahlen"""
 num_list= [1,2,3]
-#Loesung
-friends = ["Andi", "Michi", "Anna"]
+
 
 #Aufgabe 2 (Ausgabe von Listen)
 """ Erstellen sie eine Liste mit dem Namen "friends" die 3 strings enthaelt. 
 Die drei strings sind Namen: Andi, Michi, Anna
 Benutzten sie die print() Funktion um den kompletten Inhalt der Liste an zu 
 zeigen"""
-#Loesung
-friends = ["Andi", "Michi", "Anna"]
-print(friends)
+
 
 #Aufgabe 3 (Ausgabe von einzelnen Listen-Elementen)
 """Erstellen sie eine Liste mit dem Namen "friends" die 3 strings enthaelt. 
@@ -28,9 +25,6 @@ Hier ist ein Beispiel, wo das erste Element zu einer Variablen zugewiesen wird
 num_list=[1,2,3]
 num_1=num_list[0]
 print(num_1)
-#Loesung
-friends = ["Andi", "Michi", "Anna"]
-name_1 = friends[1]
 
 
 #Aufgabe 4 (List-Slicing)
@@ -44,19 +38,13 @@ Namen aus der Liste "friends""""
 sen="Hello World"
 sen_reduced=sen[0:3]
 print(sen_reduced)
-#Loesung
-friends = ["Andi", "Michi", "Anna"]
-friends_reduced = friends[0:2]
-#alternativ
-friends_reduced = friends[:2]
+
 
 #Aufgabe 5 (List manipulation)
 """ Verwenden sie die liste "friends" aus Aufgabe 1. 
 Aendern Sie nun den 2ten Namen in der Liste zu "Stefan, ohne dabei die
 komplette Liste neu zu definieren"""
 friends=["Andi", "Michi", "Anna"]
-#Loesung
-friends[1] = "Stefan"
 
 
 #Aufgabe 6 (Laenge von Listen
@@ -65,9 +53,6 @@ die Laenge von beiden Listen mit print ausgeben. Verwenden sie die len()
 Funktion"""
 friends_m= ["Andi", "Michi", "Tom"]
 friends_f= ["Anna", "Julia"]
-#Loesung
-print("Laenge der ersten Liste: " + str(len(friends_m)))
-print("Laenge der zweiten Liste: " + str(len(friends_f)))
 
 
 #Aufgabe 7 (Vereinen von Listen)
@@ -80,8 +65,7 @@ str_1="Hello "
 str_2="World"
 all_str=str_1 + str_2
 print(all_str)
-#Loesung
-all_friends = friends_m + friends_f
+
 
 #Aufgabe 7.1:
 """Gegeben ist eine while-Schleife die Eingaben vom User einliest bis er den 
@@ -89,13 +73,11 @@ String "fertig" eingibt. Ihre Aufgabe ist nun jede dieser Eingaben in eine
 Liste zu Speichern. Die Eingabe "fertig" wird nicht in die Liste geschrieben.
 Zur Hilfe steht der folgende Code der angepasst werden kann
 """
-#Loesung
 list_input=[] #erstellt eine leere Liste die befuellt wird
 while True:
     zeile= input("Neue Eingabe:\n")
     if zeile == "fertig":
         break
-    list_input.append(zeile)
     print("Ihre Eingabe:"+zeile)
 print("Skript ist beendet")
 
@@ -106,20 +88,13 @@ for-Schleife die jeden Namen in der Konsole anzeigt. Hier ist ein Beispiel"""
 num_list=[1,"a",3]
 for ii in num_list:
     print(ii)
-    
-#Loesung
-friends = ["Andi", "Michi", "Anna"]
-for ii in friends:
-    print(ii)
    
     
 #Aufgabe 9 (List summation)
 """Erstellen Sie eine Liste mit ganzen Zahlen, die von 0 bis 999 geht.
 Tip: range, Aufgabe 7.1, for-Schleife
 Bonus fuer die Pros: Loesen Sie die Aufgabe ohne eine Schleife zu verwenden"""    
-ganze_zahlen=[]
-for ii in range(1000):
-    ganze_zahlen.append(ii)
+   
  
 #Aufgabe 10 
 """ Erstellen Sie eine Funktion mit dem Namen sum_even, die "n" als input 
@@ -127,13 +102,7 @@ parameter hat. Die Funktion gibt die Summe zurueck von den geraden Zahlen
 zwischen 0 bis n. Wenn die Funktion steht, verwenden sie n=1000 und lassen Sie
 sich das Ergebniss ausgeben mit print().
 Bonus fuer die Pros: Loesen Sie die Aufgabe, ohne eine Schleife zu verwenden"""   
-#Loesung
-def sum_even(n):
-    sum=0
-    for ii in range(0,n+1,2):
-        sum = sum + ii
-    return(sum)
-summe = sum_even(10)
+
 
 #Aufgabe 11 (Elementwise addition)
 """ Gegeben sind die zwei listen "list1" und "list2". Addieren Sie die beiden 
@@ -143,10 +112,6 @@ ersten Liste mit dem zweiten Element der zweiten Liste, usw.
 Bonus fuer die Pros: Loesen Sie die Aufgabe ohne eine Schleife zu verwenden""" 
 list1=[1,2,3]
 list2=[3,4,5]
-#Loesung
-list_summe=[]
-for ii in range(len(list1)):
-    list_summe.append(list1[ii] + list2[ii])
 
 
 #Aufgabe 12 (Elemente in einer Liste finden)
@@ -157,8 +122,6 @@ Wert 8 vorkommt. Falls ja lassen Sie sich ein True ausgeben, falls nicht dann
 ein False
 """
 list_find=["a","c", 1,3,4,"ab","abc"]
-#Loesung
-"ab" in list_find
 
 
 #Aufgabe 13 (Verschachtelte Listen)
@@ -176,8 +139,7 @@ element=list_bsp[7]
 print(element)
 
 list_con=[1,2,3,[4,5,6,[7,8]],9]
-#Loesung
-wert = list_con[3][3][1]
+
 
 #Aufgabe 14 (Aufgabe 1.5A-C aus der VBA Aufgabensammlung 3.7)
 """Erstellen Sie einen Code der Ihnen die vorgegebenen Ergebnisse erzeugt. 
